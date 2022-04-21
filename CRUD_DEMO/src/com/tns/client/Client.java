305232207bc5588610238a11bc9534b1a48a6d6f
Madhujya Dutta <madhujyadutta007@gmail.com>
2022-04-21 13:22:22 +0530

    Case Study

---
+++ CRUD_DEMO/src/com/tns/client/Client.java
@@ -0,0 +1,35 @@
+package com.tns.client;
+
+import com.tns.entity.Student;
+import com.tns.service.StudentService;
+import com.tns.service.StudentServiceImpl;
+public class Client {
+public static void main(String[] args) {
+
+	StudentService service = new StudentServiceImpl();
+	Student student = new Student();
+	 
+	     student.setStudentId(120);
+         student.setName("Madhu");
+         
+         service.addStudent(student);
+         student = service.findStudentById(100);
+         
+         System.out.print("ID:"+student.getStudentId());
+         System.out.println(" Name:"+student.getName());
+
+          student = service.findStudentById(100);
+          student.setName("Madhujya Dutta");
+          service.updateStudent(student);
+          student = service.findStudentById(100);
+          
+          
+          System.out.print("ID:"+student.getStudentId());
+          System.out.println(" Name:"+student.getName());
+
+          //student = service.findStudentById(100);
+          //service.removeStudent(student);
+          
+          System.out.println("End of program/Life cycle completed...");
+}
+}
